e66b3c7489fda57d4957827e52f7b303c34bf1a9
Luan Henrique <luan.h.n.n.13@gmail.com>
2024-09-06 17:50:27 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,63 @@
+let dados = [
+    {
+      titulo: "Super Mario Odyssey",
+      descricao: "Embarque em uma aventura mundial com Mario! Neste novo jogo da série, Mario usa seu chapéu, Cappy, para se transformar em diversos objetos e criaturas, explorando mundos coloridos e desafiadores. Desvende os mistérios do reino de Bowser e salve a Princesa Peach.",
+      link: "https://www.metacritic.com/game/switch/super-mario-odyssey/",
+      tags: "aventura plataforma 3d mundo-aberto mario nintendo coletar"
+    },
+    {
+      titulo: "Mario Kart 8 Deluxe",
+      descricao: "Corra em pistas alucinantes e compita com seus personagens favoritos da Nintendo em emocionantes corridas de kart. Com power-ups divertidos e modos de jogo variados, Mario Kart 8 Deluxe é diversão garantida para todas as idades.",
+      link: "https://www.metacritic.com/game/switch/mario-kart-8-deluxe/",
+      tags: "corrida kart party multiplayer nintendo competitivo"
+    },
+    {
+      titulo: "Animal Crossing: New Horizons",
+      descricao: "Crie sua própria ilha paradisíaca e viva aventuras relaxantes com seus amigos animais. Construa sua casa, explore a ilha, pesque, colete frutas e muito mais. Uma experiência única e personalizada.",
+      link: "https://www.metacritic.com/game/switch/animal-crossing-new-horizons/",
+      link: "https://www.metacritic.com/game/switch/animal-crossing-new-horizons/",
+      tags: "simulação vida-virtual construção social nintendo relaxante"
+    },
+    {
+      titulo: "The Legend of Zelda: Breath of the Wild",
+      descricao: "Um mundo aberto imenso e cheio de mistérios espera por você. Explore Hyrule, resolva quebra-cabeças, enfrente inimigos poderosos e descubra a história de Link. Uma experiência épica para os fãs de Zelda.",
+      link: "https://www.metacritic.com/game/switch/the-legend-of-zelda-breath-of-the-wild/",
+      tags: "aventura mundo-aberto rpg ação exploração zelda open-world nintendo"
+    },
+    {
+      titulo: "Super Smash Bros. Ultimate",
+      descricao: "O maior crossover de personagens da Nintendo! Lute contra seus personagens favoritos em batalhas épicas. Com modos de jogo variados e um elenco gigantesco, Super Smash Bros. Ultimate é diversão garantida para todos os fãs de jogos de luta.",
+      link: "https://www.metacritic.com/metacritic.com/game/switch/super-smash-bros-ultimate/",
+      tags: "luta plataforma party nintendo crossover multiplayer"
+    },
+    {
+      titulo: "Pokémon Sword/Shield",
+      descricao: "Embarque em uma nova aventura em uma região repleta de Pokémon. Capture, treine e lute com seus pokémon favoritos. Descubra novas criaturas e explore um mundo aberto cheio de possibilidades.",
+      link: "https://www.metacritic.com/game/switch/pokemon-sword",
+      tags: "rpg aventura capturar pokemon nintendo mundo-aberto"
+    },
+    {
+      titulo: "Pokémon Scarlet/Violet",
+      descricao: "A nova geração de Pokémon chegou! Explore um mundo aberto vibrante e cheio de aventuras. Capture novos Pokémon, personalize seu personagem e faça novos amigos.",
+      link: "https://www.metacritic.com/game/switch/pokemon-scarlet",
+      tags: "rpg aventura capturar pokemon nintendo mundo-aberto"
+    },
+    {
+      titulo: "Splatoon 3",
+      descricao: "Mergulhe em batalhas de tinta emocionantes com seus amigos! Personalize seu personagem, escolha suas armas e domine o campo de batalha. Splatoon 3 traz novas armas, mapas e modos de jogo.",
+      link: "https://www.metacritic.com/game/switch/splatoon-3",
+      tags: "tiro multiplayer online nintendo competitivo tinta"
+    },
+    {
+      titulo: "Kirby and the Forgotten Land",
+      descricao: "Kirby está de volta em uma nova aventura 3D! Aspire inimigos, copie suas habilidades e explore um mundo esquecido. Uma aventura divertida e cheia de desafios para todas as idades.",
+      link: "https://www.metacritic.com/game/switch/kirby-and-the-forgotten-land",
+      tags: "plataforma aventura 3d nintendo Kirby copiar-habilidades"
+    },
+    {
+      titulo: "Mario Party Superstars",
+      descricao: "Mario Party Superstars é uma verdadeira celebração da série Mario Party, trazendo de volta alguns dos tabuleiros e minigames mais queridos dos primeiros títulos da franquia para o Nintendo Switch.",
+      link: "https://www.metacritic.com/game/mario-party-superstars/",
+      tags: "party nintendo mario minigames tabuleiro multiplayer"
+    }
+  ];
